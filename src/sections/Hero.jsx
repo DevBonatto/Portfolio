82dbe4d63a4ed264cca18444eaabde44d976940f
Lucca Bonatto <devbonatto@gmail.com>
2025-02-20 20:24:46 -0300

    :lipstick: feat: changing name & placing portfolio icon

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -55,7 +55,7 @@ const Hero = () => {
   return (
     <section className='min-h-screen w-full flex flex-col relative'>
       <div className='w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3'>
-        <p className='sm:text-3xl text-2xl font-medium text-white text-center font-generalsans'>Hi, I am Lucca <span className='waving-hand'>👋</span></p>
+        <p className='sm:text-3xl text-2xl font-medium text-white text-center font-generalsans'>Hi, I am Lucca Bonatto<span className='waving-hand'>👋</span></p>
         <p className='hero_tag text-gray_gradient'>Full-Stack Developer</p>
       </div>
 
